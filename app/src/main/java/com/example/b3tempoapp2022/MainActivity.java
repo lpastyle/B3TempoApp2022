7037728af699f89777639f9f6c00fe7820c0e07c
lpastyle <lpastorelli@lpastyle.com>
2023-01-03 11:38:33 +0100

    Added notification channel creation

--- app/src/main/java/com/example/b3tempoapp2022/MainActivity.java
+++ app/src/main/java/com/example/b3tempoapp2022/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.b3tempoapp2022;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +23,8 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String CHANNEL_ID = "tempo_alert_channel_id";
+
     public static IEdfApi edfApi;
     ActivityMainBinding binding;
 
@@ -96,7 +101,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void createNotificationChannel() {
-
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
   /*  public void showHistory(View view) {
