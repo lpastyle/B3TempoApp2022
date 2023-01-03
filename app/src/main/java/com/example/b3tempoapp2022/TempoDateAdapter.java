3da10c6142a66564380d761624a179ee013e4ded
lpastyle <lpastorelli@lpastyle.com>
2023-01-03 09:11:24 +0100

    Added view binding in wiew holder

--- app/src/main/java/com/example/b3tempoapp2022/TempoDateAdapter.java
+++ app/src/main/java/com/example/b3tempoapp2022/TempoDateAdapter.java
@@ -12,6 +12,8 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.b3tempoapp2022.databinding.TempoDateItemBinding;
+
 import java.util.List;
 
 public class TempoDateAdapter extends RecyclerView.Adapter<TempoDateAdapter.TempoDateViewHolder> {
@@ -31,13 +33,22 @@ public class TempoDateAdapter extends RecyclerView.Adapter<TempoDateAdapter.Temp
     @Override
     public TempoDateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tempo_date_item, parent,false);
-        return new TempoDateViewHolder(v);
+        TempoDateItemBinding binding = TempoDateItemBinding.bind(v);
+        return new TempoDateViewHolder(binding);
+
+        // old way of proceeding when using findViewById
+        //View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tempo_date_item, parent,false);
+        //return new TempoDateViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TempoDateViewHolder holder, int position) {
-         holder.dateTv.setText(tempoDates.get(position).getDate());
-         holder.colorFl.setBackgroundColor(ContextCompat.getColor(context, tempoDates.get(position).getCouleur().getResId()));
+        holder.binding.dateTv.setText(tempoDates.get(position).getDate());
+        holder.binding.colorFl.setBackgroundColor(ContextCompat.getColor(context, tempoDates.get(position).getCouleur().getResId()));
+
+        // old way of proceeding with findViewById
+        //holder.dateTv.setText(tempoDates.get(position).getDate());
+        //holder.colorFl.setBackgroundColor(ContextCompat.getColor(context, tempoDates.get(position).getCouleur().getResId()));
     }
 
     @Override
@@ -45,14 +56,25 @@ public class TempoDateAdapter extends RecyclerView.Adapter<TempoDateAdapter.Temp
         return tempoDates.size();
     }
 
+    /**
+     * Old way of proceeding with 'findViewById'
+     *
+     public class TempoDateViewHolder extends RecyclerView.ViewHolder {
+     TextView dateTv;
+     FrameLayout colorFl;
+     public TempoDateViewHolder(@NonNull View itemView) {
+     super(itemView);
+     dateTv = itemView.findViewById(R.id.date_tv);
+     colorFl = itemView.findViewById(R.id.color_fl);
+     }
+     }
+     */
     public class TempoDateViewHolder extends RecyclerView.ViewHolder {
-        TextView dateTv;
-        FrameLayout colorFl;
+        TempoDateItemBinding binding;
 
-        public TempoDateViewHolder(@NonNull View itemView) {
-            super(itemView);
-            dateTv = itemView.findViewById(R.id.date_tv);
-            colorFl = itemView.findViewById(R.id.color_fl);
+        public TempoDateViewHolder(@NonNull TempoDateItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 
